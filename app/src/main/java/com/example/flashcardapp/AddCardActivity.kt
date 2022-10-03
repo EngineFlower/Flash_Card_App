d4f7af3cc78ea1d36a72b42f201b7ec39da5db57
EngineFlower <jasmineayesha@gmail.com>
2022-10-03 19:14:17 -0400

    added ability to add new question and answer

--- app/src/main/java/com/example/flashcardapp/AddCardActivity.kt
+++ app/src/main/java/com/example/flashcardapp/AddCardActivity.kt
@@ -1,8 +1,10 @@
 package com.example.flashcardapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class AddCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +16,20 @@ class AddCardActivity : AppCompatActivity() {
             //dismiss add question activity
             finish()
         }
+        //click listener for save button
+        findViewById<View>(R.id.save_button).setOnClickListener{
+
+            //Creating a new intent
+            val data = Intent()
+            //puts new question into the Intent
+            data.putExtra("add_question", findViewById<EditText>(R.id.add_question).text.toString())
+            //puts answer into Intent
+            data.putExtra("answer", findViewById<EditText>(R.id.add_answer).text.toString())
+
+            setResult(RESULT_OK, data)
+            //dismiss add card activity
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/flashcardapp/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.flashcardapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,15 +31,32 @@ class MainActivity : AppCompatActivity() {
             //Makes answer invisible
             findViewById<View>(R.id.flash_answer).visibility = View.INVISIBLE
         }
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data: Intent? = result.data
+            if (data != null) { // Check that we have data returned
+                val string1 = data.getStringExtra("add_question") // 'string1' needs to match the key we used when we put the string in the Intent
+                val string2 = data.getStringExtra("answer")
+
+                // Log the value of the strings for easier debugging
+                val questionText = findViewById<TextView>(R.id.flash_question)
+                questionText.text = string1
+
+                val answerText = findViewById<TextView>(R.id.flash_answer)
+                answerText.text = string2
+
+            } else {
+                Log.i("MainActivity", "Returned null data from AddCardActivity")
+            }
+        }
 
         //Adds click listener that takes user to add_card_activity
         findViewById<View>(R.id.add_question_button).setOnClickListener{
             val intent = Intent(this, AddCardActivity::class.java)
-            startActivity(intent)
-
+                resultLauncher.launch(intent)
 
 
         }
+
     }
 
 }
